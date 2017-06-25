d3002adb0c2cae6bd103fbb505c41367d5e8080d
Brennan Glynn <brennanglynn@u.boisestate.edu>
2017-06-25 13:29:08 -0600

    Added react to webpack project

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-document.write('Hello World!')
+import React, { Component } from 'react'
+import ReactDom from 'react-dom'
+
+const styles = {
+  app: {
+    paddingTop: 40,
+    textAlign: 'center',
+  }
+}
+
+class App extends Component {
+  render () {
+    return (
+      <div style={styles.app}>
+        Welcome to React!
+      </div>
+    )
+  }
+}
+
+const root = document.querySelector('#app')
+ReactDom.render(<App />, root)
