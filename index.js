3a90054690e537a8477652597aaa4f8015c0d9f7
Brennan Glynn <brennanglynn@u.boisestate.edu>
2017-06-25 15:02:08 -0600

    Child components

--- index.js
+++ index.js
@@ -8,15 +8,41 @@ const styles = {
   }
 }
 
-class App extends Component {
+class Card extends Component {
+  render () {
+    const style = {
+      margin: 20,
+      padding: 20,
+      color: 'white',
+      backgroundColor: this.props.color
+    }
+    return (
+      <div style={style}>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+class Sub extends Component {
   render () {
     return (
-      <div style={styles.app}>
-        Welcome to React!
+      <div>
+        <img src={'//unsplash.it/200/300'} />
       </div>
     )
   }
 }
 
 const root = document.querySelector('#app')
-ReactDom.render(<App />, root)
+
+const element = (
+  <div>
+    <Card color={'skyblue'}>Card 1</Card>
+    <Card color={'steelblue'}>Card 2</Card>
+  </div>
+)
+
+const sub = document.querySelector('#sub')
+ReactDom.render(element, root)
+ReactDom.render(<Sub />, sub)
