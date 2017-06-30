c9055c5f773a3ff12b30b8e3f9f99d5a4532dd1d
Brennan Glynn <brennanglynn@u.boisestate.edu>
2017-06-29 23:55:35 -0600

    Removed everything

--- index.js
+++ index.js
@@ -1,63 +1,5 @@
 import React, { Component } from 'react'
 import ReactDom from 'react-dom'
-import styled from 'styled-components'
-
-const randomColor = () => '#' + Math.random().toString(16).substr(-6)
-
-const Lard = styled.div`
-  padding: 20px;
-  text-align: center;
-  color: white;
-  background-color: ${props => props.color};
-`
-
-const Container = styled.div`
-  padding: 20px;
-  margin: 20px;
-  color: white;
-  background-color: #0693e3;
-`
-
-class Card extends Component {
-  render () {
-    const style = {
-      margin: 20,
-      padding: 20,
-      color: 'white',
-      backgroundColor: this.props.color
-    }
-    return (
-      <div style={style}>
-        {this.props.children}
-      </div>
-    )
-  }
-}
-
-class Counter extends Component {
-  state = {
-    count: 1,
-    color: 'skyblue'
-  }
-
-  randomizeColor = () => this.setState({ color: randomColor() })
-
-  handleClick = () => {
-    const { count } = this.state
-    this.setState({ count: count + 1})
-  }
-
-  render () {
-    const { count, color } = this.state
-
-    return (
-    <Container>
-      <div onClick={this.handleClick}>Click here to increment {count}
-      </div>
-    </Container>
-    )
-  }
-}
 
 const root = document.querySelector('#app')
 
@@ -70,9 +12,7 @@ class App extends Component {
     }
     return (
       <div>
-        <Card color={'skyblue'}>Card 1</Card>
-        <Card color={'steelblue'}>Card 2</Card>
-        <Counter/>
+        hello world
       </div>
     )
   }
